19116a4573c33fb2941285c423401e682acf6afd
Abdi Pranoto <abdi.p43@gmail.com>
2023-12-28 17:58:40 +0700

    fillable image tag

--- app/Http/Controllers/RenterController.php
+++ app/Http/Controllers/RenterController.php
@@ -6,7 +6,6 @@ use App\Models\renter;
 use App\Models\renter_document;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class RenterController extends Controller
 {
